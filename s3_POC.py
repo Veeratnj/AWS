231e4d4a52e773d824e34919e033af7589c1f4fd
Veeratnj <124329716+Veeratnj@users.noreply.github.com>
2024-07-17 17:48:09 +0530

    Create s3_POC.py
    initial commit

---
+++ s3_POC.py
@@ -0,0 +1,17 @@
+import boto3
+
+def s3_upload(file_name, bucket_name=os.getenv('S3_BUCKET_NAME')):
+    aws_access_key_id = os.getenv('S3_ACCESS_KEY')
+    aws_secret_access_key = os.getenv('S3_SECRET_KEY')
+
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+    try:
+        print(file_name)
+        s3.upload_file(file_name, bucket_name, f"DuplicateFiles/{file_name}")
+        
+    except Exception as e:
+        print('error is ',e)
